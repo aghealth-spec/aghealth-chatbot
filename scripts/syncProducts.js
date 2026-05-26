1e7dca130a0e96d48e94effd32bc3930548dc705
aghealth <aghealth@ag-health.com>
2026-05-26 19:11:15 +0900

    add faq expiration_dt to chatbot

--- scripts/syncProducts.js
+++ scripts/syncProducts.js
@@ -57,7 +57,7 @@ async function syncProducts() {
         VALUES (
           $1,$2,$3,$4,$5,
           $6,$7,$8,$9,
-          $10,$11,true,NOW(),$12
+          $10,$11,true,NOW(),$12,$13
         )
         ON CONFLICT (goods_no)
         DO UPDATE SET
